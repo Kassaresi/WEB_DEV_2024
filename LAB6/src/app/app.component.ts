0cb7e65e4e3a32a170c14c30acbfb1552bb4475c
pp2-22B030477 <122406043+Kassaresi@users.noreply.github.com>
2024-03-13 22:53:01 +0600

    LAB6

---
+++ LAB6/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from './about/about.component';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterModule, HomeComponent, AboutComponent]
+})
+export class AppComponent {
+  title = 'Kassi Album';
+}
